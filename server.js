a4ec0b1c4de21f17e1f71ff3a8194890738048cd
Casa JeanPierre <jean05343@gmail.com>
2022-01-16 18:26:43 -0500

    Subiendo Version 1.0 WEBSERVER

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/parciales');
+
+//motor de busquedas
+app.set('view engine', 'hbs');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        titulo: 'Home',
+        nombre: "JeAn PIerrE",
+
+    });
+});
+app.get('/acerca', (req, res) => {
+    res.render('acerca', {
+        titulo: "Acerca"
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto 3000 ${ port }`);
+});
